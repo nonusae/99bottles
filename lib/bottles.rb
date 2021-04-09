cebce47a3144a874bfeff287e41c86488abf8391
nonusae <nonusae@gmail.com>
2021-04-09 21:31:55 +0700

    Implment successor concept

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,24 +9,40 @@ class Bottles
   end
 
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-    "#{quantity(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
-    "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+    case number
+    when 0
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
+      "#{action(number)}, " +
+      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
+    else
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
+      "#{action(number)}, " +
+      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
+    end
   end
 
   private
 
+  def successor(number)
+    if number.zero?
+      99
+    else
+      number - 1
+    end
+  end
+
   def action(number)
-    if number == 0
-      "Go to the store and buy some more"
+    if number.zero?
+      'Go to the store and buy some more'
     else
       "Take #{pronoun(number)} down and pass it around"
     end
   end
 
   def quantity(number)
-    if number == 0
+    if number.zero?
       'no more'
     else
       number.to_s
