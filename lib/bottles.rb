5efe06ecfdb589ff275a33e1f65acbf5b7164751
nonusae <nonusae@gmail.com>
2021-04-13 16:05:09 +0700

    Use Hash creation for bottle factory method instead

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -22,11 +22,13 @@ class BottleNumber
   attr_reader :number
 
   def self.for(number)
-    begin
-      const_get("BottleNumber#{number}")
-    rescue
-      BottleNumber
-    end.new(number)
+    Hash
+      .new(BottleNumber)
+      .merge({
+        0 => BottleNumber0,
+        1 => BottleNumber1,
+        6 => BottleNumber6
+      })[number].new(number)
   end
 
   def initialize(number)
