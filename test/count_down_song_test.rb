bce3ec408642781df664385dcb3da0abb5cea16d
nonusae <nonusae@gmail.com>
2021-04-15 16:26:47 +0700

    Use VerseFake to simplify CounDownSongTest and make it tell better story for class

--- test/count_down_song_test.rb
+++ test/count_down_song_test.rb
@@ -4,37 +4,34 @@ require 'minitest/pride'
 require_relative '../lib/count_down_song'
 
 class CountDownSongTest < Minitest::Test
-  def test_a_couple_verses
-    expected =
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
-      "\n" +
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
-    assert_equal expected, CountDownSong.new.verses(99, 98)
+  class VerseFake
+    def self.lyrics(number)
+      "This is verse #{number}.\n"
+    end
   end
 
-  def test_a_few_verses
+  def test_verse
     expected =
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n" +
-      "\n" +
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n" +
-      "\n" +
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    assert_equal expected, CountDownSong.new.verses(2, 0)
+      "This is verse 500.\n"
+
+    assert_equal(
+      expected,
+      CountDownSong.new(verse_template: VerseFake).verse(500)
+    )
+  end
+
+  def test_verses
+    expected =
+    "This is verse 99.\n" +
+    "\n" +
+    "This is verse 98.\n" +
+    "\n" +
+    "This is verse 97.\n"
+
+    assert_equal(
+      expected,
+      CountDownSong.new(verse_template: VerseFake).verses(99, 97)
+    )
   end
 
   def test_the_whole_song
